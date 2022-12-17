92e0d5643fea72917a4b27aec38687719d2e6390
კრიპტოლუქსი <73051326+Ghonghito@users.noreply.github.com>
2022-12-17 21:04:09 +0400

    Update Button

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -6,10 +6,19 @@ const Index = ({ color, textColor, contained, children, className = '', ...rest 
 		<div>
 			{contained ?
 				(
-					<button {...rest} className={`border-2 border-${color} px-4 py-2 text-${textColor} rounded-lg duration-150 hover:scale-95 hover:bg-${color} shadow-lg hover:shadow-${color}/40`}>{children}</button>)
+					<button
+						{...rest}
+						className={`border-[1px] border-${color} px-4 py-2 text-primary hover:text-white rounded-lg duration-150 hover:scale-[0.99] w-full hover:bg-${color} shadow-lg hover:shadow-${color}/40 disabled:bg-gray-500 disabled:text-gray-700 disabled:cursor-not-allowed disabled:scale-100 disabled:border-0 ${className}`}>
+						{children}
+					</button>
+				)
 				:
 				(
-					<button {...rest} className={`bg-${color} px-4 py-2 text-${textColor} rounded-lg duration-150 hover:scale-[0.99] hover:bg-primaryDark shadow-lg w-full hover:shadow-${color}/40 ${className}`}>{children}</button>
+					<button
+						{...rest}
+						className={`bg-${color} px-4 py-2 text-${textColor} rounded-lg duration-150 hover:scale-[0.99] hover:bg-primaryDark shadow-lg w-full hover:shadow-${color}/40 disabled:bg-gray-500 disabled:text-gray-700 disabled:cursor-not-allowed disabled:scale-100 ${className}`}>
+						{children}
+					</button>
 				)}
 		</div>
 	)

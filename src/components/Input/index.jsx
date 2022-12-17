d1b158a189dfb5f51b4376fb2df66cf7ff8829e4
კრიპტოლუქსი <73051326+Ghonghito@users.noreply.github.com>
2022-12-17 21:12:37 +0400

    Update Input

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const index = ({ className = '', ...rest }) => {
   return (
-    <input {...rest} className={`dark:bg-[#181818] border border-[#e3e3e6] dark:border-[#303031] rounded-lg p-2 w-full text-lightText dark:text-[#868687] focus:border focus:outline-none placeholder:text-[#4c4c4d] autofill:bg-yellow-200' ${className}`} />
+    <input
+      {...rest}
+      className={`dark:bg-[#181818] border border-[#e3e3e6] dark:border-[#303031] rounded-lg p-2 w-full text-lightText dark:text-[#868687] focus:outline-none placeholder:text-[#727274]' ${className}`} />
   )
 }
 

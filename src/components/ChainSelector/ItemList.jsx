e71d5f0d9cac267da85cfc8e4e815ceb1ae09142
კრიპტოლუქსი <73051326+Ghonghito@users.noreply.github.com>
2022-12-18 22:42:10 +0400

    update chain selector

--- src/components/ChainSelector/ItemList.jsx
+++ src/components/ChainSelector/ItemList.jsx
@@ -3,8 +3,8 @@ import Avatar from '../Avatar'
 
 const ItemList = ({ name, logo, color, logoWidth }) => {
 	return (
-		<div className='flex items-center duration-75 p-2 hover:bg-lightHover dark:hover:bg-darkHover rounded-lg cursor-pointer'>
-			<div className={`bg-${color} w-[30px] h-[30px] mr-2 rounded-lg flex justify-center items-center`}>
+		<div className='group flex items-center duration-75 p-2 hover:bg-lightHover dark:hover:bg-darkHover rounded-lg cursor-pointer'>
+			<div className={`${color} w-[30px] h-[30px] mr-2 rounded-lg flex justify-center items-center`}>
 				<Avatar src={logo} alt={name} className={`${logoWidth}`} />
 			</div>
 			<a href='#/' className='text-lightText dark:text-darkText text-sm'>
--- src/components/ChainSelector/index.jsx
+++ src/components/ChainSelector/index.jsx
@@ -19,37 +19,37 @@ const ChainSelector = () => {
 	const { chainId } = useWeb3React()
 	return (
 		<Menu as='div' className='inline-block text-left'>
-			<Menu.Button className='p-2 w-full rounded-md bg-primary hover:bg-primaryDark font-medium text-white duration-150 hover:scale-95'>
+			<Menu.Button className='p-2 w-full rounded-md bg-white dark:bg-darkBorder hover:bg-lightHover dark:hover:bg-darkHover font-medium shadow-md text-white duration-150 hover:scale-[0.98]'>
 				<div className='flex items-center justify-between gap-2'>
 					<div>
 						{getChainFullName(chainId)}
 					</div>
-					<FiChevronDown className='h-5 w-5' aria-hidden='true' />
+					<FiChevronDown className='h-5 w-5 duration-150 text-zinc-800 dark:text-darkText' aria-hidden='true' />
 				</div>
 			</Menu.Button>
 			<Transition as={Fragment} enter='transition ease-out duration-100' enterFrom='transform opacity-0 scale-95' enterTo='transform opacity-100 scale-100' leave='transition ease-in duration-75' leaveFrom='transform opacity-100 scale-100' leaveTo='transform opacity-0 scale-95'>
 				<Menu.Items className='origin-top-center absolute center-1 mt-2 w-36 rounded-md shadow-lg z-50 bg-white dark:bg-darkCard focus:outline-none'>
 					<ul className='p-1'>
 						<li onClick={() => BNBChain()}>
-							<ItemList name='Smart Chain' logo={BSC} logoWidth='w-5' color='primary' />
+							<ItemList name='Smart Chain' logo={BSC} logoWidth='w-5' color='bg-yellow-500' />
 						</li>
 						<li onClick={() => ETHChain()}>
-							<ItemList name='Ethereum' logo={ETH} logoWidth='w-3' color='primary' />
+							<ItemList name='Ethereum' logo={ETH} logoWidth='w-3' color='bg-blue-500' />
 						</li>
 						<li onClick={() => AvalancheChain()}>
-							<ItemList name='Avalanche' logo={AVAX} logoWidth='w-4' color='primary' />
+							<ItemList name='Avalanche' logo={AVAX} logoWidth='w-4' color='bg-red-500' />
 						</li>
 						<li onClick={() => PolygonChain()}>
-							<ItemList name='Polygon' logo={Polygon} logoWidth='w-4' color='primary' />
+							<ItemList name='Polygon' logo={Polygon} logoWidth='w-4' color='bg-purple-500' />
 						</li>
 						<li onClick={() => FantomChain()}>
-							<ItemList name='Fantom' logo={Fantom} logoWidth='w-3' color='primary' />
+							<ItemList name='Fantom' logo={Fantom} logoWidth='w-3' color='bg-blue-400' />
 						</li>
 						<li onClick={() => CronosChain()}>
-							<ItemList name='Cronos' logo={Cronos} logoWidth='w-4' color='primary' />
+							<ItemList name='Cronos' logo={Cronos} logoWidth='w-4' color='bg-blue-800' />
 						</li>
 						<li onClick={() => ArbitrumChain()}>
-							<ItemList name='Arbitrum' logo={Arbitrum} logoWidth='w-4' color='primary' />
+							<ItemList name='Arbitrum' logo={Arbitrum} logoWidth='w-4' color='bg-blue-600' />
 						</li>
 					</ul>
 				</Menu.Items>

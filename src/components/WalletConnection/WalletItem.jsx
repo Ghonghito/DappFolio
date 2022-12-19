a5eb6b9f15cde9966f92d64671fb05a1bd335c3a
კრიპტოლუქსი <73051326+Ghonghito@users.noreply.github.com>
2022-12-19 19:57:29 +0400

    update connect wallet button

--- src/components/WalletConnection/WalletItem.jsx
+++ src/components/WalletConnection/WalletItem.jsx
@@ -4,10 +4,10 @@ import Avatar from 'components/Avatar'
 const WalletItem = ({ name, icon }) => {
   return (
     <li>
-      <a href='#/' className='group flex items-center p-3 text-base font-bold bg-gray-300 dark:bg-darkCard rounded-lg duration-200 hover:bg-primary dark:hover:bg-neutral-800'>
+      <a href='#/' className='group flex items-center p-3 text-base font-bold bg-lightBorder dark:bg-darkBorder rounded-lg duration-200 hover:bg-lightHover dark:hover:bg-darkHover'>
         <Avatar src={icon} alt='' className='w-5' />
-        <div className='flex-1 ml-3 text-lightText dark:text-darkText group-hover:text-white'>{name}</div>
-        {name === 'Metamask' ? (<span className='inline-flex items-center justify-center px-2 py-0.5 ml-3 text-xs text-white  font-medium bg-primary rounded-md shadow shadow-dark'>პოპულარული</span>) : null}
+        <div className='flex-1 ml-3 text-lightText dark:text-darkText dark:group-hover:text-white'>{name}</div>
+        {name === 'Metamask' ? (<span className='inline-flex items-center justify-center px-2 py-0.5 ml-3 text-xs text-white  font-medium bg-primary rounded-md shadow shadow-primary'>პოპულარული</span>) : null}
       </a>
     </li>
   )

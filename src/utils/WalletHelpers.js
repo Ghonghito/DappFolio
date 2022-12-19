707a0903da76e1ed1e1d512c82229a208c93a238
კრიპტოლუქსი <73051326+Ghonghito@users.noreply.github.com>
2022-12-19 13:56:49 +0400

    removed web3 functions from WalletHelpers.js

--- src/utils/WalletHelpers.js
+++ src/utils/WalletHelpers.js
@@ -1,5 +1,3 @@
-import Web3 from 'web3'
-import { getSimpleCoinPrice } from './APIs/CoinGeckoAPI';
 import Avatar from '../components/Avatar'
 import BSC from '../assets/images/Blockchains/Binance.svg'
 import ETH from '../assets/images/Blockchains/Ethereum.svg'
@@ -8,12 +6,6 @@ import FANTOM from '../assets/images/Blockchains/Fantom.svg'
 import MATIC from '../assets/images/Blockchains/Matic.svg'
 import Cronos from '../assets/images/Blockchains/Cronos.svg'
 import Arbitrum from '../assets/images/Blockchains/Arbitrum.svg'
-
-const web3 = new Web3(window.ethereum);
-
-const web3BSC = new Web3('https://bsc-dataseed1.binance.org/');
-const web3ETH = new Web3('https://rpc.ankr.com/eth');
-const web3AVAX = new Web3('https://api.avax.network/ext/bc/C/rpc');
 
 const explorerURLS = {
   56: 'https://bscscan.com',
@@ -106,7 +98,6 @@ export const getChainFullName = (chainId) => {
         <div className='bg-yellow-500 p-1 rounded-lg'>
           <Avatar src={BSC} alt='bsc' className='w-4' />
         </div>
-        {/* <Avatar src={BSC} alt='bsc' className='w-4' /> */}
         <p className='font-semibold text-sm hidden md:flex text-zinc-600 dark:text-zinc-300'>Smart Chain</p>
         <p className='font-semibold text-sm flex md:hidden text-zinc-600 dark:text-zinc-300'>BSC</p>
       </div>
@@ -173,26 +164,5 @@ export const getExplorerURL = (type, data, chain) => {
       return `${explorerURLS[chain]}/block/${data}`;
     default:
       return `${explorerURLS[chain]}/`;
-  }
-}
-
-export const getNativeBalance = async (account) => {
-  const nativeBalance = await web3.eth.getBalance(account);
-  return web3.utils.fromWei(nativeBalance, 'ether');
-}
-
-export const getBalancesOnSupportedChains = async (account) => {
-  const BNBPrice = await getSimpleCoinPrice('binancecoin')
-  const ETHPrice = await getSimpleCoinPrice('ethereum')
-  const AVAXPrice = await getSimpleCoinPrice('avalanche-2')
-
-  const balanceBSC = await web3BSC.eth.getBalance(account);
-  const balanceETH = await web3ETH.eth.getBalance(account);
-  const balanceAVAX = await web3AVAX.eth.getBalance(account);
-
-  return {
-    bsc: { bal: web3BSC.utils.fromWei(balanceBSC, 'ether'), usd: Number(BNBPrice) * Number(web3BSC.utils.fromWei(balanceBSC, 'ether')) },
-    eth: { bal: web3ETH.utils.fromWei(balanceETH, 'ether'), usd: Number(ETHPrice) * Number(web3ETH.utils.fromWei(balanceETH, 'ether')) },
-    avax: { bal: web3AVAX.utils.fromWei(balanceAVAX, 'ether'), usd: Number(AVAXPrice) * Number(web3AVAX.utils.fromWei(balanceAVAX, 'ether')) }
   }
 }

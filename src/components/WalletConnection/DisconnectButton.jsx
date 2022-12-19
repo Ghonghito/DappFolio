133f912f0ba259e912238038e04f90f19f7e811f
კრიპტოლუქსი <73051326+Ghonghito@users.noreply.github.com>
2022-12-19 19:32:27 +0400

    update DisconnectButton Design

--- src/components/WalletConnection/DisconnectButton.jsx
+++ src/components/WalletConnection/DisconnectButton.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import Modal from '../Modal'
 import MetamaskIcon from 'assets/images/WalletIcons/MetamaskIcon.svg'
 import TrustWalletIcon from 'assets/images/WalletIcons/TrustWalletIcon.svg'
+import Typtography from 'components/Typography'
 import { FiExternalLink, FiCopy } from 'react-icons/fi'
 import { useWeb3React } from '@web3-react/core'
 import { useToast } from 'hooks/useToast'
@@ -32,15 +33,15 @@ export default function DisconnectButton() {
 		<div>
 			<div className='flex items-center gap-2'>
 				<ChainSelector />
-				<button onClick={() => setOpen(true)} className='bg-primary text-white p-2 text-sm  rounded-lg font-semibold flex items-center gap-2 duration-150 hover:scale-95 hover:bg-primaryDark'>
-					{wallet === 'Metamask' ? <img src={MetamaskIcon} alt='mm' className='w-4' /> : <img src={TrustWalletIcon} alt='mm' className='w-4' />}
-					{shortAddress(account, 5)}
+				<button onClick={() => setOpen(true)} className='flex items-center justify-center font-bold text-zinc-600 dark:text-zinc-300 gap-2 p-2 w-full rounded-md bg-white dark:bg-darkBorder hover:bg-lightHover dark:hover:bg-darkHover shadow-md duration-150 hover:scale-[0.98]'>
+					{wallet === 'Metamask' ? <img src={MetamaskIcon} alt='mm' className='w-5' /> : <img src={TrustWalletIcon} alt='mm' className='w-4' />}
+					{shortAddress(account, 4)}
 				</button>
 			</div>
 			<Modal title='თქვენი საფულე' open={open} close={() => setOpen(!open)}>
-				<div className='px-3'>
-					<div className='border border-indigo-200 dark:border-gray-400 w-full rounded-xl p-3'>
-						<p className='text-sm font-semibold text-lightText dark:text-darkText'>დაკავშირებულია - {wallet}</p>
+				<div className='p-3'>
+					<div className='border border-lightBorder dark:border-darkBorder w-full rounded-xl p-3'>
+						<Typtography className='text-sm font-semibold'>დაკავშირებულია - {wallet}</Typtography>
 						<div className='flex items-center gap-2 py-3'>
 							<p className='text-lightText dark:text-darkText font-bold text-sm'>{shortAddress(account, 10)}</p>
 						</div>
@@ -56,8 +57,8 @@ export default function DisconnectButton() {
 						</div>
 					</div>
 					<div className='flex flex-row gap-2 justify-end py-3'>
-						<button onClick={() => setOpen(!open)} className='duration-150 rounded-lg text-white p-2 bg-primary hover:bg-primaryDark hover:scale-95 text-sm'>დახურვა</button>
-						<button onClick={() => walletDisconnect()} className='duration-105 hover:scale-95 bg-error text-white  rounded-lg py-2 duration-150 hover:bg-red-700 border-[1px] border-red-700 px-3 text-sm'>გამოსვლა</button>
+						<button onClick={() => setOpen(!open)} className='duration-150 rounded-lg text-white p-2 bg-primary hover:bg-primaryDark hover:scale-[0.99] text-sm'>დახურვა</button>
+						<button onClick={() => walletDisconnect()} className='duration-105 hover:scale-[0.99] bg-error text-white  rounded-lg py-2 duration-150 hover:bg-red-700 border-[1px] border-red-700 px-3 text-sm'>გამოსვლა</button>
 					</div>
 				</div>
 			</Modal>
